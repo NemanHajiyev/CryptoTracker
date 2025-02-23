c727ec99ad843bbfa57be5bee6478e1ceca24b44
NemanHajiyev <zaraganq@gmail.com>
2025-02-23 12:37:18 +0400

    Chart.js Updated

--- src/Components/LineChart/Chart.jsx
+++ src/Components/LineChart/Chart.jsx
@@ -10,6 +10,7 @@ const Chart = ({ coinId, coinName }) => {
     const [chartData, setChartData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [days, setDays] = useState(7);
+    const [selectChart, setSelectChart] = useState("prices");
 
     useEffect(() => {
         const fetchChartData = async () => {
@@ -19,15 +20,16 @@ const Chart = ({ coinId, coinName }) => {
                 const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${days}`);
                 const data = await res.json();
 
-                const prices = data.prices.map(price => price[1]);
-                const labels = data.prices.map(price => new Date(price[0]).toLocaleDateString());
+                const selectedData = data[selectChart] || [];
+                const labels = selectedData.map(entry => new Date(entry[0]).toLocaleDateString());
+                const values = selectedData.map(entry => entry[1]);
 
                 setChartData({
                     labels: labels,
                     datasets: [
                         {
-                            label: `${coinName} Price Chart`,
-                            data: prices,
+                            label: `${coinName} Chart`,
+                            data: values,
                             borderColor: "rgba(75,192,192,1)",
                             backgroundColor: "rgba(192, 75, 75, 0.2)",
                             borderWidth: 2,
@@ -45,20 +47,28 @@ const Chart = ({ coinId, coinName }) => {
         };
 
         fetchChartData();
-    }, [coinId, days]);
+    }, [coinId, days, selectChart]);
 
     return (
         <div className="chart-container">
+            <div className="selection-div">
+                <select className="select" value={days} onChange={(e) => setDays(Number(e.target.value))}>
+                    <option value="7">7 - Day</option>
+                    <option value="30">30 - Day</option>
+                    <option value="60">60 - Day</option>
+                    <option value="90">90 - Day</option>
+                </select>
 
-            <select value={days} onChange={(e) => setDays(Number(e.target.value))}>
-                <option value="7">7 Days</option>
-                <option value="30">30 Days</option>
-                <option value="60">60 Days</option>
-                <option value="90">90 Days</option>
-            </select>
+                <select className="select" value={selectChart} onChange={(e) => setSelectChart(e.target.value)}>
+                    <option value="prices">Price</option>
+                    <option value="total_volumes">Total Volume</option>
+                    <option value="market_caps">Market Cap</option>
+                </select>
+            </div>
+
             {isLoading ? <Loading /> : (
                 <div>
-                    <h2>{coinName} Price Chart - (Last {days} Days)</h2>
+                    <h2>{coinName} {selectChart.replace("_", " ")} Chart - (L ast {days} Days )</h2>
                     <Line data={chartData} />
                 </div>
             )}
